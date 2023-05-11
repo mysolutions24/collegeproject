cfd4674a8ca7baf5e9d7544055045549350a1ab3
MUDASSIR AHMED <101457011+mysolutions24@users.noreply.github.com>
2023-05-11 14:18:42 +0530

    Update info.py
    hide SMTP email and password

--- backend/info.py
+++ backend/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'mudassirkhaan39@gmail.com'
-EMAIL_HOST_PASSWORD = 'qxtefmyibccgwkto'
+EMAIL_HOST_USER = 'example@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT =587
